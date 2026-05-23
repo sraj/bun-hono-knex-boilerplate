a59a44374924ce758fac8bdee8ee512b4a9d3d13
sraj <sumanraj.v@gmail.com>
2026-05-23 00:02:27 -0700

    format fixes

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -2,7 +2,8 @@ import process from 'node:process';
 
 import pino from 'pino';
 
-const logFormat = process.env.LOG_FORMAT ?? (process.stdout.isTTY ? 'pretty' : 'json');
+const logFormat =
+  process.env.LOG_FORMAT ?? (process.stdout.isTTY ? 'pretty' : 'json');
 
 export const logger = pino({
   ...(logFormat === 'pretty'
